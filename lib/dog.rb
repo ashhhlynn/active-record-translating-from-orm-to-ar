8f018101040d5fd633c55aa8afdceb279c307834
ashhhlynn <ashleylynnwelch@gmail.com>
2022-05-31 15:35:45 -0500

    commit tree c25bde2c8241afbd4f793ff889316d5729c26c34

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,26 @@
-class Dog
+class Dog < ActiveRecord::Base
+attr_accessor :name, :breed 
+
+def self.create(name:, breed:)
+    Dog.new 
+end 
+
+
+def self.save 
+    self.save 
+
+end 
+
+def self.update
+end 
+
+def self.find_by_name
+end 
+
+def self.find_by_id
+end 
+
+def self.find_by(name)
+end 
+
 end
